fd7ee8e738badc81fa1e57c38afd70842724c2e7
Mozgovoy Sergey <brainstable@gmail.com>
2024-01-22 20:38:31 +0300

    Сompleting task 2 in topic 1

---
+++ Topic 1/lesson_2.py
@@ -0,0 +1,6 @@
+firstNumber = int(input('Введите 1-е число: '))
+secondNumber = int(input('Введите 2-е число: '))
+
+average = (firstNumber + secondNumber) / 2
+
+print(f'Среднее арифметическое чисел {firstNumber} и {secondNumber} равно {average}')
