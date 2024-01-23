73ca7e799db7c0d121f647ac6d0d07749cab8f69
Mozgovoy Sergey <brainstable@gmail.com>
2024-01-23 21:47:00 +0300

    Сompleting lesson_3_3

---
+++ Topic 3/lesson_3_3.py
@@ -0,0 +1,11 @@
+lst = [10, 17, 13, 44, 23, 88, 100, 99]
+
+print('Четные числа:')
+for i in lst:
+    if i % 2 == 0:
+        print(i)
+
+print('Нечетные числа:')
+for i in lst:
+    if i % 2 != 0:
+        print(i)
