51d1d1ca181309c678114ae4884fc501a4a43cf1
Mozgovoy Sergey <brainstable@gmail.com>
2024-01-23 22:13:06 +0300

    Сompleting lesson_3_4

---
+++ Topic 3/lesson_3_4.py
@@ -0,0 +1,12 @@
+for i in range(1, 101):
+    if i % 2 == 0:
+        print(i)
+
+for i in range(2, 101, 2):
+    print(i)
+
+n = 100
+counter = 2
+while counter <= n:
+    print(counter)
+    counter += 2
